abf468486d72df65228be892e08b93b0ac336c30
sankarg <sankarmca55@gmail.com>
2024-03-22 17:47:45 +0530

    Puls survey Android application

---
+++ src/main/java/PlusSurvey_Mobile_Application/PlusSurvey_Mobile_Application/App.java
@@ -0,0 +1,13 @@
+package PlusSurvey_Mobile_Application.PlusSurvey_Mobile_Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
